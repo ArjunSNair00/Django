77a90236b1af8f8225cdaa4327a3e868be173c7d
Arjun S Nair <snairarjun43@gmail.com>
2025-08-13 22:56:51 +0530

    awf

--- calculator/settings.py
+++ calculator/settings.py
@@ -12,18 +12,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
-
-import os
-
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MIDDLEWARE = [
-    "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
-    ...
-]
-
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
